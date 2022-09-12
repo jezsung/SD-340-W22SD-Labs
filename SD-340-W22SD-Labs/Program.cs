0d3dc435b91f1ea530f7f180d94331c1f45dfe32
Jaesung Hong <jezsung@gmail.com>
2022-09-12 23:42:53 +0900

    add delegates

--- SD-340-W22SD-Labs/Program.cs
+++ SD-340-W22SD-Labs/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+WordNumber concatenate = new WordNumber(Concatenate);
+WordNumber removeLetters = new WordNumber(RemoveLetters);
+
+
+Console.WriteLine(concatenate("Hello", 3));
+Console.WriteLine(removeLetters("Hello", 3));
+
+
+string Concatenate(string wordArg, int numArg)
+{
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < numArg; i++)
+    {
+        sb.Append(wordArg);
+    }
+
+    return sb.ToString();
+}
+
+string RemoveLetters(string wordArg, int numArg)
+{
+    if (wordArg.Length <= numArg)
+    {
+        return "";
+    }
+
+    return wordArg.Remove(wordArg.Length - numArg, numArg);
+}
+
+delegate string WordNumber(string wordArg, int numArg);
